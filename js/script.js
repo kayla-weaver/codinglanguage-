c7d208a92e0dc2cc950a446436b94d2e24f6a7e1
Kayla Weaver <kaylabeth1211@gmail.com>
2023-07-01 15:30:29 -0700

    mess around with javascript trying to get code to work.

--- js/script.js
+++ js/script.js
@@ -1,20 +1,29 @@
 window.onload = function() {
     const form = document.querySelector("form");
     document.querySelector("form").onsubmit = function(event) {
-      event.preventDefault();
-      hideResults();
-    const sparkle = document.getElementById("sprakle").value;
+     
+      // hideResults();
+    const sparkle = document.getElementById("sparkle").value;
     const creepy= document.getElementById("creepy").value;
     const coffee = document.getElementById("coffee").value;
     const question = document.getElementById("question").value;
     const other = document.getElementById("other").value;
-    const selectElement=document.getElementById("sparkles");
-    const yes= document.getElementById('input').value;
-    if (spakle= yes) {
-    document.getElementById("Ruby").removeAttribute("class");
+    // const selectElement=document.getElementById("sparkle");
+    const yes= document.getElementById('option').value;
+    const no= document.getElementById('option').value;
+    let ruby = document.getElementById("Ruby");
+    ruby.setAttribute("class","hidden");
+    let javascript = document.getElementById("JavaScript");
+    javascript.setAttribute("class","hidden");
+    let python =document.getElementById("Python");
+    python.setAttribute("class","hidden");
+    if (sparkle=== yes && creepy=== yes && coffee=== yes && question=== yes && other=== yes) {
+    ruby.removeAttribute("class");
+    } else if (sparkle=== no && creepy=== no && coffee=== no && question=== no && other=== no) { 
+      javascript.removeAttribute("class");
+    } else {
+     python.removeAttribute("class"); 
+    event.preventDefault();
     }}
-  function hideResults(){
-    document.getElementById("JavaScript").setAttribute("class","hidden");
-    document.getElementById("Python").setAttribute("class","hidden");
-    document.getElementById("Ruby").setAttribute("class","hidden");
-  }};
+  // function hideResults(){
+  };
