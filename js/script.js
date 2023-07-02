f43b1a812636df24f2f1c5e96d6e0bdb7a2ab0e7
Kayla Weaver <kaylabeth1211@gmail.com>
2023-07-01 19:36:13 -0700

    finally get all of the code to work together to do what I'm asking it to do.

--- js/script.js
+++ js/script.js
@@ -1,31 +1,46 @@
-window.onload = function() {
-    const form = document.querySelector("select-form");
-    document.querySelector(form).onsubmit = function(event) {
-      // hideResults();
+window.onload = function () {
+  let form = document.querySelector("form#select-form");
+  // console.log("form", form);
+  form.onsubmit = function (event) {
+    // hideResults();
+    console.log("on submit working")
     const sparkle = document.getElementById("sparkle").value;
-    const creepy= document.getElementById("creepy").value;
+    const creepy = document.getElementById("creepy").value;
     const coffee = document.getElementById("coffee").value;
     const question = document.getElementById("question").value;
     const other = document.getElementById("other").value;
     // const selectElement=document.getElementById("sparkle");
-    const yes= document.getElementById('yes').value;
-    console.log(yes)
-    const no= document.getElementById('no').value;
+    // const yes = document.getElementById("yes").value;
+    // console.log(yes);
+    // const no = document.getElementById("no").value;
     let ruby = document.getElementById("Ruby");
-    ruby.setAttribute("class","hidden");
+    ruby.setAttribute("class", "hidden");
     let javascript = document.getElementById("JavaScript");
-    javascript.setAttribute("class","hidden");
-    let python =document.getElementById("Python");
-    python.setAttribute("class","hidden");
-    if (sparkle=== yes && creepy=== yes && coffee=== yes && question=== yes && other=== yes) {
-    ruby.removeAttribute("class");
-    } else if (sparkle=== no && creepy=== no && coffee=== no && question=== no && other=== no) { 
+    javascript.setAttribute("class", "hidden");
+    let python = document.getElementById("Python");
+    python.setAttribute("class", "hidden");
+    let yes= "yes";
+    let no = "no";
+    if (
+      sparkle === yes &&
+      creepy === yes &&
+      coffee === yes &&
+      question === yes &&
+      other === yes
+    ) {
+      ruby.removeAttribute("class");
+    } else if (
+      sparkle === no &&
+      creepy === no &&
+      coffee === no &&
+      question === no &&
+      other === no
+    ) {
       javascript.removeAttribute("class");
     } else {
-    python.removeAttribute("class"); 
+      python.removeAttribute("class");
+    }
     event.preventDefault();
-    }}
-  // function hideResults(){
   };
-
-  
+  // function hideResults(){
+};
